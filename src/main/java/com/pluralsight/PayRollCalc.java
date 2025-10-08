c5e7fd9e43391fb31d4287f68d92f857edcecb79
AJ V <avaithyam@yahoo.com>
2025-10-08 10:31:48 -0400

    employee class set up

---
+++ src/main/java/com/pluralsight/PayRollCalc.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class PayRollCalc {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fileReader = new FileReader("employees.csv");
+
+    }
+}
